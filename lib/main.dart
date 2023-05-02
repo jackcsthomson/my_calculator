81985c81ffeb886d0cd47ff3ef5b9419a979d3dd
Jack Thomson <jack.thomson5515@gmail.com>
2023-05-01 21:01:25 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -10,57 +10,94 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Calculator',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  /*void _incrementCounter() {
     setState(() {
       _counter++;
     });
+  }*/
+
+  static const List<List<String>> lon = [
+    ['7', '8', '9'],
+    ['4', '5', '6'],
+    ['1', '2', '3']
+  ];
+
+  Widget buttonBuilder(
+      String title, Color buttonColor, Color textColor, double h, double w) {
+    return Container(
+      height: h,
+      width: w,
+      color: buttonColor,
+      child: Center(
+        child: TextButton(
+          onPressed: () {
+            print(title);
+          },
+          child: SizedBox(
+            height: h,
+            width: w,
+            child: Center(
+              child: Text(
+                title,
+                style: TextStyle(color: textColor),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            for (List<String> L in lon)
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  for (String s in L)
+                    buttonBuilder(
+                        s, Colors.grey, Colors.white, height / 4, width / 3)
+                ],
+              ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                buttonBuilder(
+                    '0', Colors.grey, Colors.white, height / 4, width * 2 / 3),
+                buttonBuilder(
+                    '=', Colors.grey, Colors.white, height / 4, width / 3)
+              ],
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
